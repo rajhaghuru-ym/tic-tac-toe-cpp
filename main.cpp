503da982cff0c8ed567b9eb8656a909a45d1d0b3
rajhaghuru-ym <rajhaghuru.offc@gmail.com>
2026-04-21 09:02:37 +0530

    UC2: Toss to decide first player and assign symbols

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    char board[3][3];
+
+    // UC1: Initialize board
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = '-';
+        }
+    }
+
+    // Display board
+    cout << "Tic-Tac-Toe Board:\n\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // UC2: Toss
+    srand(time(0));
+    int toss = rand() % 2;
+
+    char userSymbol, computerSymbol;
+    bool isUserTurn;
+
+    if (toss == 0) {
+        cout << "\nYou won the toss! You play first.\n";
+        userSymbol = 'X';
+        computerSymbol = 'O';
+        isUserTurn = true;
+    } else {
+        cout << "\nComputer won the toss! Computer plays first.\n";
+        userSymbol = 'O';
+        computerSymbol = 'X';
+        isUserTurn = false;
+    }
+
+    cout << "Your Symbol: " << userSymbol << endl;
+    cout << "Computer Symbol: " << computerSymbol << endl;
+
+    if (isUserTurn)
+        cout << "Current Turn: User\n";
+    else
+        cout << "Current Turn: Computer\n";
+
+    return 0;
+}
